e2e8ccb3e231a02e224ba4ce65f469e6beb2c6dd
margarcac1 <margarcac1@alum.us.es>
2024-11-10 19:38:03 +0000

    feat: Add Patient schema
    The commit adds a new file `Patient.js` which defines the schema for the `Patient` model in the application. The schema includes fields for `name`, `surname`, `birthdate`, `dni`, `city`, `clinicHistoryId`, and `userId`. The `dni` field has validation logic to ensure it is a valid DNI number. The commit also includes necessary imports and exports.

---
+++ src/schemas/Patient.js
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+import { validate as uuidValidate, v4 as uuidv4 } from 'uuid';
+
+const patientSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    default: () => uuidv4(),
+    validate: {
+      validator: uuidValidate,
+      message: (props) => `${props.value} is not a vaid UUID`,
+    },
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  surname: {
+    type: String,
+    required: true
+  },
+  birthdate: {  
+    type: Date,
+    required: true
+  },
+  dni: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function(v) {
+        const dniRegex = /^[0-9]{8}[A-Z]$/;
+        if (!dniRegex.test(v)) {
+          return false;
+        }
+        const letters = 'TRWAGMYFPDXBNJZSQVHLCKE';
+        const number = v.slice(0, 8);
+        const letter = v.slice(8, 9);
+        return letters[number % 23] === letter;
+      },
+      message: props => `${props.value} is not a valid DNI number!`
+    }
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  clinicHistoryId: {
+    type: String,
+    required: true
+  },
+  userId: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+const Patient = mongoose.models.Patient || mongoose.model('Patient', patientSchema);
+export default Patient;
