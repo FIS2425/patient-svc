47943489dd7f24a911e9e9575b9e5cc154f9639c
manuelsanjim <manusj0712@correo.ugr.es>
2024-11-11 20:06:18 +0100

    feat: Add patient retrieval and deletion endpoints; update patient registration

--- src/controllers/patientController.js
+++ src/controllers/patientController.js
@@ -5,7 +5,7 @@ import logger from '../config/logger.js';
 
 export const register = async (req, res) => {
   try {
-    const { name, surname,birthdate, dni, city, clinicHistoryId, username, password, email} = req.body;
+    const { name, surname, birthdate, dni, city, clinicHistoryId, username, password, email } = req.body;
     try {
 
       const patient = new Patient({
@@ -13,10 +13,10 @@ export const register = async (req, res) => {
         surname,
         birthdate,
         dni,
-        city, 
-        clinicHistoryId, 
-        username, 
-        password, 
+        city,
+        clinicHistoryId,
+        username,
+        password,
         email,
         // userId: authResponse.data._id
         userId: '18e373c7-092a-1720-a381-fd909g52153'
@@ -40,3 +40,46 @@ export const register = async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 }
+
+export const obtainAll = async (req, res) => {
+  try {
+    const patients = await Patient.find(); // Encuentra todos los pacientes
+    res.status(200).json(patients);
+  } catch (error) {
+    logger.error('Error fetching patients', {
+      method: req.method,
+      url: req.originalUrl,
+      error: error
+    });
+    res.status(500).json({ message: error.message });
+  }
+}
+
+export const deletePatient = async (req, res) => {
+
+  try {
+    const { id } = req.params;
+    const patient = await Patient.findById(id);
+    if (!patient) {
+      logger.error('Patient not found', {
+        method: req.method,
+        url: req.originalUrl
+      });
+      res.status(404).json({ message: 'Patient not found' });
+    } else {
+      try {
+        await patient.deleteOne();
+        logger.info(`Patient ${patient._id} deleted from database`);
+        res.status(204).json({ message: 'Patient deleted' });
+      } catch (error) {
+        logger.error('Error deleting user', {
+          method: req.method,
+          url: req.originalUrl,
+          error: error
+        });
+      }
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
--- src/routes/patientRoute.js
+++ src/routes/patientRoute.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { register } from '../controllers/patientController.js';
+import { register,obtainAll,deletePatient} from '../controllers/patientController.js';
 
 const router = express.Router();
 
 router.post('/register', register);
+router.get('/obtainAll', obtainAll);
+router.delete('/deletePatient/:id', deletePatient);
 
 export default router;
